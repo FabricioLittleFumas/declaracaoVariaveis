4970492d0d498042c1258ec8832707c80579a7b3
FabricioGit2024 LittleFumas <fabriciogit2024@hotmail.com>
2024-07-19 19:28:08 -0300

    armazenando variaveis do tipo doouble e boolean

--- src/util/declaracao/DeclaracaoPrincipal.java
+++ src/util/declaracao/DeclaracaoPrincipal.java
@@ -3,31 +3,47 @@ package util.declaracao;
 public class DeclaracaoPrincipal {
 
 	public static void main(String[] args) {
-		int idade = 10;
-		System.out.println(idade);
+//		int idade = 10;
+//		System.out.println(idade);
 		//comentario de uma única linha
 		/*
 		comentario de mais de uma linha
 		*/
 		//aqui vou declarar uma variável idade e atruir valor a mesma
-		int idd = 10;
-		System.out.println(idd);
-		
-		int idadeAnoQueVem = idade + 1;
-		System.out.println(idadeAnoQueVem);
+//		int idd = 10;
+//		System.out.println(idd);
+//		
+//		int idadeAnoQueVem = idade + 1;
+//		System.out.println(idadeAnoQueVem);
 		
 		
 		//atribuicao de valores e utilizacao de operadores em variaveis em Java
 		
-		int cinquenta = 10+40;
-		int cinquentaMinus = 100-50;
-		int cinquentaMulti = 2*25;
-		int cinquentaDiv = 100/2;
+//		int cinquenta = 10+40;
+//		int cinquentaMinus = 100-50;
+//		int cinquentaMulti = 2*25;
+//		int cinquentaDiv = 100/2;
+//		
+//		System.out.println(cinquenta);
+//		System.out.println(cinquentaMinus);
+//		System.out.println(cinquentaMulti);
+//		System.out.println(cinquentaDiv);
 		
-		System.out.println(cinquenta);
-		System.out.println(cinquentaMinus);
-		System.out.println(cinquentaMulti);
-		System.out.println(cinquentaDiv);
+		//utilizadao de variavel do tipo double
+		double valor = 3.14;
+		System.out.println(valor);
+		double x = 10 * 2; 
+		System.out.println(x);
+		
+		//variavel do tipo boolean armazena verdadeiro ou falso
+		boolean vrdd = true;
+		System.out.println(vrdd);
+		int idade = 15;
+		boolean maiorDeIdade = idade >= 18;
+		System.out.println(maiorDeIdade);
+		
+		
+		
 		
 	}
 }
