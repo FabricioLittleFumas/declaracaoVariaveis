e3ae8f9b3907a7fe9b0f3f2f5d33bc5f63d581d6
FabricioGit2024 LittleFumas <fabriciogit2024@hotmail.com>
2024-07-19 19:18:32 -0300

    comentarios e atribuicao de valores em variaveis

--- src/util/declaracao/DeclaracaoPrincipal.java
+++ src/util/declaracao/DeclaracaoPrincipal.java
@@ -5,5 +5,17 @@ public class DeclaracaoPrincipal {
 	public static void main(String[] args) {
 		int idade = 10;
 		System.out.println(idade);
+		//comentario de uma única linha
+		/*
+		comentario de mais de uma linha
+		*/
+		//aqui vou declarar uma variável idade e atruir valor a mesma
+		int idd = 10;
+		System.out.println(idd);
+		
+		int idadeAnoQueVem = idade + 1;
+		System.out.println(idadeAnoQueVem);
+		
+		
 	}
 }
